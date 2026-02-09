be1a4b045b1b04b35727d649d84158c558bbd9ad
jpaschal87 <jpaschal87@gmail.com>
2026-02-09 07:41:25 -0600

    Create words.js

---
+++ words.js
@@ -0,0 +1,23 @@
+// Replace/expand these lists with the full Fry sets as you like.
+window.FRY_LISTS = [
+  {
+    id: "fry_1_25",
+    name: "Fry 1–25",
+    words: ["the","of","and","a","to","in","is","you","that","it","he","was","for","on","are","as","with","his","they","I","at","be","this","have","from"]
+  },
+  {
+    id: "fry_26_50",
+    name: "Fry 26–50",
+    words: ["or","one","had","by","words","but","not","what","all","were","we","when","your","can","said","there","use","an","each","which","she","do","how","their","if"]
+  },
+  {
+    id: "fry_51_75",
+    name: "Fry 51–75",
+    words: ["will","up","other","about","out","many","then","them","these","so","some","her","would","make","like","him","into","time","has","look","two","more","write","go","see"]
+  },
+  {
+    id: "fry_76_100",
+    name: "Fry 76–100",
+    words: ["number","no","way","could","people","my","than","first","water","been","call","who","oil","its","now","find","long","down","day","did","get","come","made","may","part"]
+  }
+];
